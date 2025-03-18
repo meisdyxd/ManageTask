b69571b8a178dee0bc818937a8ce2516c1812dbd
meisdyxd <kararturkar@gmail.com>
2025-03-18 21:57:18 +0300

    Реализованы репозитории всех сущностей(их всего две, умом)

--- ManageTask.Infrastructure/Data/Repositories/TaskRepository.cs
+++ ManageTask.Infrastructure/Data/Repositories/TaskRepository.cs
@@ -1,33 +1,102 @@
 ﻿using ManageTask.Application.Abstractions.Data;
+using ManageTask.Domain;
+using ManageTask.Infrastructure.Data.Contexts;
+using ManageTask.Infrastructure.Data.Entities;
+using ManageTask.Infrastructure.Data.Mappers;
+using Microsoft.Extensions.Logging;
 using ResultSharp.Core;
+using ResultSharp.Errors;
+using ResultSharp.Extensions.FunctionalExtensions.Sync;
 
 namespace ManageTask.Infrastructure.Data.Repositories
 {
-    public class TaskRepository : ITaskRepository
+    public class TaskRepository(DataContext context, ILogger<TaskRepository> logger) : ITaskRepository
     {
-        public Task<Result<Domain.Task>> AddAsync(Domain.Task entity, CancellationToken cancellationToken)
+        private readonly ILogger<TaskRepository> logger = logger;
+        private readonly DataContext context = context;
+        public async Task<Result<Domain.Task>> AddAsync(Domain.Task task, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Добавление задачи с ID: {TaskId}", task.Id);
+
+            var entity = GetTaskEntity(task);
+            await context.AddAsync(entity, cancellationToken);
+            await context.SaveChangesAsync();
+
+            logger.LogInformation("Задача с ID: {TaskId} добавлена", entity.Id);
+            return entity.Map();
         }
 
-        public Task<Result> DeleteAsync(Guid id, CancellationToken cancellationToken)
+        public async Task<Result> DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Удаление задачи с ID: {TaskId}", id);
+
+            var entity = await context.Tasks.FindAsync(keyValues: [id], cancellationToken: cancellationToken);
+            if(entity is null)
+            {
+                logger.LogWarning("Задача с ID: {TaskId} не найдена для удаления", id);
+                return Error.NotFound($"Задача с ID: {id} не найдена");
+            }
+            context.Tasks.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Задача с ID: {TaskId} успешно удалена", id);
+            return Result.Success();
         }
 
         public Result<IQueryable<Domain.Task>> GetAll()
         {
-            throw new NotImplementedException();
+            logger.LogInformation($"Получение всех задач");
+            return Result<IQueryable<Domain.Task>>.Success(context.Tasks.Select(u => u.Map()).AsQueryable());
         }
 
-        public Task<Result<Domain.Task>> GetAsync(Guid id, CancellationToken cancellationToken)
+        public async Task<Result<Domain.Task>> GetAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Получение задачи с ID: {TaskId}", id);
+
+            var entity = await context.Tasks.FindAsync(keyValues: [id], cancellationToken: cancellationToken);
+            if (entity is null)
+            {
+                logger.LogWarning("Задача с ID: {TaskId}, для получения не найдена", id);
+                return Error.NotFound($"Задача с ID: {id} не найдена");
+            }
+
+            logger.LogInformation("Задача с ID: {TaskId} получена", id);
+            return entity.Map();
+        }
+        public Result<IQueryable<Domain.Task>> GetByIdCreatorAsync(Guid id, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Получение задач с ID создателя: {UserId}", id);
+
+            var tasks = context.Tasks.Where(t => t.CreatedById == id).Select(t => t.Map()).AsQueryable();
+
+            logger.LogInformation("Задачи с ID создателя: {UserId} получены", id);
+            return Result<IQueryable<Domain.Task>>.Success(tasks);
+        }
+        public Result<IQueryable<Domain.Task>> GetByIdAssignedAsync(Guid id, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Получение задач с ID назначенного пользователя: {UserId}", id);
+
+            var tasks = context.Tasks.Where(t => t.AssignedToId == id).Select(t => t.Map()).AsQueryable();
+
+            logger.LogInformation("Задачи с ID назначеноого пользователя: {UserId} получены", id);
+            return Result<IQueryable<Domain.Task>>.Success(tasks);
         }
 
-        public Task<Result<Domain.Task>> UpdateAsync(Domain.Task entity, CancellationToken cancellationToken)
+        public async Task<Result<Domain.Task>> UpdateAsync(Domain.Task task, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Обновление задачи с ID: {TaskId}", task.Id);
+
+            var entity = task.Map();
+            context.Tasks.Update(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Задача с ID: {TaskId} успешно обновлена", entity.Id);
+            return entity.Map();
         }
+
+        public TaskEntity GetTaskEntity(Domain.Task task)
+        {
+            return task.Map();
+        } 
     }
 }
--- ManageTask.Infrastructure/Data/Repositories/UserRepository.cs
+++ ManageTask.Infrastructure/Data/Repositories/UserRepository.cs
@@ -1,34 +1,98 @@
 ﻿using ManageTask.Application.Abstractions.Data;
 using ManageTask.Domain;
+using ManageTask.Infrastructure.Data.Contexts;
+using ManageTask.Infrastructure.Data.Entities;
+using ManageTask.Infrastructure.Data.Mappers;
+using Microsoft.Extensions.Logging;
 using ResultSharp.Core;
+using ResultSharp.Errors;
 
 namespace ManageTask.Infrastructure.Data.Repositories
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository(DataContext context, ILogger<UserRepository> logger) : IUserRepository
     {
-        public Task<Result<User>> AddAsync(User entity, CancellationToken cancellationToken)
+        private readonly ILogger<UserRepository> logger = logger;
+        private readonly DataContext context = context;
+
+        public async Task<Result<User>> AddAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Добавление пользователя с ID: {UserId}", user.Id);
+            var entity = GetUserEntity(user);
+            await context.Users.AddAsync(entity, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Пользователь успешно добавление с ID: {UserId}", entity.Id);
+            return entity.Map();
         }
 
-        public Task<Result> DeleteAsync(Guid id, CancellationToken cancellationToken)
+        public async Task<Result> DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Удаление пользователя с ID: {UserId}", id);
+            var entity = await context.Users.FindAsync(keyValues: [id], cancellationToken: cancellationToken);
+
+            if(entity is null)
+            {
+                logger.LogWarning("Пользователь с ID: {UserId}, для удаления не найден", id);
+                return Error.NotFound($"Пользователь с ID: {id} не найден");
+            }
+
+            context.Users.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Пользователь с ID: {UserId} удалён", id);
+            return Result.Success();
         }
 
         public Result<IQueryable<User>> GetAll()
         {
-            throw new NotImplementedException();
+            logger.LogInformation($"Получение всех пользователей");
+            return Result<IQueryable<User>>.Success(context.Users.Select(u => u.Map()).AsQueryable());
         }
 
-        public Task<Result<User>> GetAsync(Guid id, CancellationToken cancellationToken)
+        public async Task<Result<User>> GetAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Получение пользователя с ID: {UserId}", id);
+
+            var entity = await context.Users.FindAsync(keyValues: [id], cancellationToken: cancellationToken);
+            if (entity is null)
+            {
+                logger.LogWarning("Пользователь с ID: {UserId}, для получения не найден", id);
+                return Error.NotFound($"Пользователь с ID: {id} не найден");
+            }
+
+            logger.LogInformation("Пользователь с ID: {UserId} получен", id);
+            return entity.Map();
+        }
+        public async Task<Result<User>> GetByEmailAsync(string email, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Получение пользователя с почтой: {Email}", email);
+
+            var entity = await context.Users.FindAsync(keyValues: [email], cancellationToken: cancellationToken);
+            if (entity is null)
+            {
+                logger.LogWarning("Пользователь с почтой: {Email}, для получения не найден", email);
+                return Error.NotFound($"Пользователь с почтой: {email} не найден");
+            }
+
+            logger.LogInformation("Пользователь с почтой: {Email} получен", email);
+            return entity.Map();
         }
 
-        public Task<Result<User>> UpdateAsync(User entity, CancellationToken cancellationToken)
+        public async Task<Result<User>> UpdateAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("Обновление пользователя с ID: {UserId}", user.Id);
+
+            var entity = user.Map();
+            context.Users.Update(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Пользователь с ID: {UserId} успешно обновлён", entity.Id);
+            return entity.Map();
+        }
+        public UserEntity GetUserEntity(User user)
+        {
+            var entity = user.Map();
+            return entity;
         }
     }
 }
